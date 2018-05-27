675318ba2a19604df1cac66b7f9c6b548f00b834
Noam <noam@fireflymigration.com>
2018-05-27 17:02:45 +0300

    added public reference to PostgresSchemaBuilder

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
 export { ExpressBridge } from './utils/server/expressBridge';
 export { SQLServerDataProvider } from './utils/server/SQLServerDataProvider';
-export { PostgresDataProvider } from './utils/server/PostgresDataProvider';
+export { PostgresDataProvider,PostgrestSchemaBuilder } from './utils/server/PostgresDataProvider';
 
