593fbf3b2945d4343b78a106c77558d0c8999e30
Noam <noam@fireflymigration.com>
2018-06-24 17:43:07 +0300

    fixed column lookup async

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -861,7 +861,7 @@ export class Column<dataType>  {
   async  lookupAsync<lookupIdType, entityType extends Entity<lookupIdType>>(lookupEntity: entityType, filter?: Column<lookupIdType> | ((entityType: entityType) => FilterBase)): entityType {
     if (!filter)
       filter = <any>this;
-    return await  this._entity.lookupAsync(lookupEntity, filter);
+    return  this._entity.lookupAsync(lookupEntity, filter);
   }
   __isVirtual() {
     if (this.__settings && this.__settings.virtualData)
