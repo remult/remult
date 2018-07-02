5ce698d95f82679eb98325587ee603303d29c369
Noam <noam@fireflymigration.com>
2018-07-02 15:04:20 +0300

    made wrap fetch public

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,5 @@
 export * from './utils/utils';
 export { RadWebModule } from './utils/angular/radWebModule';
 export { DataProviderFactory } from './utils/dataInterfaces1';
-export { RestDataProvider,Action } from './utils/restDataProvider';
+export { RestDataProvider,Action,wrapFetch,WrapFetchInterface } from './utils/restDataProvider';
 export {LocalStorageDataProvider } from './utils/localStorageDataProvider';
