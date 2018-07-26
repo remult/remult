f8e07f692b73be819d1a0689cd55c541746f23d9
Noam <noam@fireflymigration.com>
2018-07-26 10:21:23 +0300

    Added initial transactions support

--- src/utils/server/PostgresDataProvider.ts
+++ src/utils/server/PostgresDataProvider.ts
@@ -5,57 +5,87 @@ import { FilterBase, DataProviderFactory, DataProvider, ColumnValueProvider, Dat
 
 import { DataApi, DataApiResponse } from './DataApi';
 
-import { Pool, QueryResult } from 'pg';
+import { Pool, QueryResult, Client } from 'pg';
 import { ActualSQLServerDataProvider, SQLConnectionProvider, SQLCommand, SQLQueryResult } from './SQLDatabaseShared';
 
 
 export class PostgresDataProvider implements DataProviderFactory {
 
 
-  constructor(private pool: Pool) {
+    constructor(private pool: Pool) {
 
-  }
+    }
+    provideFor<T extends Entity<any>>(name: string, factory: () => T): DataProvider {
+        return new ActualSQLServerDataProvider(factory, name, new PostgresBridgeToSQLConnection(this.pool), factory);
+    }
+
+    async doInTransaction(what: (dp: DataProviderFactory) => Promise<void>) {
+        let client = await this.pool.connect();
+        let dp = new PostgresDataTransaction(client);
+        try {
+            await client.query('BEGIN');
+            await what(dp);
+            await client.query('COMMIT');
+        }
+        catch (err) {
+            await client.query('ROLLBACK');
+            throw err;
+        }
+        finally {
+            await client.release();
+        }
+    }
+
+}
+
+class PostgresDataTransaction implements DataProviderFactory {
 
 
-  provideFor<T extends Entity<any>>(name: string, factory: () => T): DataProvider {
-    return new ActualSQLServerDataProvider(factory, name, new PostgresBridgeToSQLConnection(this.pool), factory);
-  }
+    constructor(private source: PostgresCommandSource) {
+
+    }
+    provideFor<T extends Entity<any>>(name: string, factory: () => T): DataProvider {
+        return new ActualSQLServerDataProvider(factory, name, new PostgresBridgeToSQLConnection(this.source), factory);
+    }
 
 
+}
+interface PostgresCommandSource {
+    query(queryText: string, values?: any[]): Promise<QueryResult>;
 }
 class PostgresBridgeToSQLConnection implements SQLConnectionProvider {
-  constructor(private pool: Pool) {
+    constructor(private source: PostgresCommandSource) {
 
-  }
-  createCommand(): SQLCommand {
-    return new PostgrestBridgeToSQLCommand(this.pool);
-  }
+    }
+    createCommand(): SQLCommand {
+        return new PostgrestBridgeToSQLCommand(this.source);
+    }
 }
 class PostgrestBridgeToSQLCommand implements SQLCommand {
-  constructor(private pool: Pool) {
+    constructor(private source: PostgresCommandSource) {
 
-  }
-  values: any[]=[];
-  addParameterToCommandAndReturnParameterName(col: Column<any>, val: any): string {
-    this.values.push(val);
-    return '$' + this.values.length;
-  }
-  query(sql: string): Promise<SQLQueryResult> {
-    return this.pool.query(sql, this.values).then(r => new PostgressBridgeToSQLQueryResult(r));
-  }
+    }
+    values: any[] = [];
+    addParameterToCommandAndReturnParameterName(col: Column<any>, val: any): string {
+        this.values.push(val);
+        return '$' + this.values.length;
+    }
+    query(sql: string): Promise<SQLQueryResult> {
+        return this.source.query(sql, this.values).then(r => new PostgressBridgeToSQLQueryResult(r));
+    }
 }
 class PostgressBridgeToSQLQueryResult implements SQLQueryResult {
-  getColumnIndex(name: string): number {
-    for (let i = 0; i < this.r.fields.length; i++) {
-      if (this.r.fields[i].name.toLowerCase() == name.toLowerCase())
-        return i;
+    getColumnIndex(name: string): number {
+        for (let i = 0; i < this.r.fields.length; i++) {
+            if (this.r.fields[i].name.toLowerCase() == name.toLowerCase())
+                return i;
+        }
+        return -1;
     }
-    return -1;
-  }
-  constructor(private r: QueryResult) {
-    this.rows = r.rows;
-  }
-  rows: any[];
+    constructor(private r: QueryResult) {
+        this.rows = r.rows;
+    }
+    rows: any[];
 
 }
 
