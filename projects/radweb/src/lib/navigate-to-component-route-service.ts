0c4d229715dc53c9859e80d79c4538006612fabd
Noam <noam@fireflymigration.com>
2019-07-25 19:25:59 +0300

    fixed can navigate route

--- projects/radweb/src/lib/navigate-to-component-route-service.ts
+++ projects/radweb/src/lib/navigate-to-component-route-service.ts
@@ -1,12 +1,13 @@
-import { Router, Route } from '@angular/router';
-import { Injectable } from '@angular/core';
+import { Router, Route, CanActivate, ActivatedRouteSnapshot } from '@angular/router';
+import { Injectable, Injector } from '@angular/core';
+import { Context } from 'radweb';
 @Injectable()
-export class NavigateToComponentRouteService {
-    constructor(private router: Router) {
+export class RouteHelperService {
+    constructor(private router: Router,private injector:Injector) {
 
     }
   
-    navigate(toComponent: { new(...args: any[]): any }) {
+    navigateToComponent(toComponent: { new(...args: any[]): any }) {
         let done = false;
         this.router.config.forEach(path => {
             if (done)
@@ -19,4 +20,69 @@ export class NavigateToComponentRouteService {
         if (!done)
             console.warn("couldn't find path for ", toComponent, this.router.config);
     }
+    canNavigateToRoute(route: Route) {
+        if (!route.canActivate)
+            return true;
+        for (let guard of route.canActivate) {
+            let g = this.injector.get(guard) as CanActivate;
+            if (g && g.canActivate) {
+                var r = new dummyRoute();
+                r.routeConfig = route;
+                let canActivate = g.canActivate(r, undefined);
+                if (!canActivate)
+                    return false;
+            }
+        }
+        return true;
+    }
+}
+
+@Injectable()
+export class AuthorizedGuard implements CanActivate {
+    constructor(private context: Context, private router: Router) {
+    }
+    canActivate(route: ActivatedRouteSnapshot) {
+        let allowedRoles: string[];
+
+        let data = route.routeConfig.data as AuthorizedGuardRouteData;
+        if (data && data.allowedRoles)
+            allowedRoles = data.allowedRoles;
+
+        if (this.context.hasRole(...allowedRoles)) {
+            return true;
+        }
+        if (!(route instanceof dummyRoute))
+            this.router.navigate(['/']);
+        return false;
+    }
+}
+@Injectable()
+export class NotLoggedInGuard implements CanActivate {
+    constructor(private context: Context, private router: Router) {
+    }
+    canActivate(route: ActivatedRouteSnapshot) {
+        let allowedRoles: string[];
+        if (this.context.user)
+            return false;
+        return true;
+
+    }
+}
+export interface AuthorizedGuardRouteData {
+    allowedRoles?: string[];
+    name?: string;
+
+}
+class dummyRoute extends ActivatedRouteSnapshot {
+    constructor() {
+        super();
+
+    }
+    routeConfig: any;
+}
+export interface AuthorizedGuardRoute extends Route {
+    data?: {
+        allowedRoles?: string[];
+        name?: string;
+    };
 }
