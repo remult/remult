757bfbd887704a0c75605db37dd65a2824ac7f54
Noam <noam@fireflymigration.com>
2020-01-01 13:43:46 +0200

    fix readonly together with select

--- projects/core/src/angular/data-control/data-control2.component.ts
+++ projects/core/src/angular/data-control/data-control2.component.ts
@@ -67,7 +67,7 @@ export class DataControl2Component {
 
   }
   isSelect(): boolean {
-    if (this.map.dropDown)
+    if (this.map.dropDown&&this._getEditable())
       return true;
     return false;
   }
--- projects/core/src/angular/data-control/data-control3.component.ts
+++ projects/core/src/angular/data-control/data-control3.component.ts
@@ -70,7 +70,7 @@ export class DataControl3Component {
 
   }
   isSelect(): boolean {
-    if (this.map.dropDown)
+    if (this.map.dropDown&&this._getEditable())
       return true;
     return false;
   }
