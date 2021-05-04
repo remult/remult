65242f375b9ea5b24515f82386219d2553db16d0
Noam <noam@fireflymigration.com>
2021-05-04 13:35:50 +0300

    released version 2.5.4

--- projects/angular/schematics/hello/files/src/server/index.ts
+++ projects/angular/schematics/hello/files/src/server/index.ts
@@ -12,36 +12,38 @@ import * as jwt from 'express-jwt';
 import * as compression from 'compression';
 
 import '../app/app.module';
+async function startup() {
+    config(); //loads the configuration from the .env file
+    let dataProvider: DataProvider;
 
-config(); //loads the configuration from the .env file
-let dataProvider: DataProvider;
-
-// use json db for dev, and postgres for production
-if (!process.env.DEV_MODE) {
-    const pool = new Pool({
-        connectionString: process.env.DATABASE_URL,
-        ssl: process.env.DEV_MODE ? false : { rejectUnauthorized: false }// use ssl in production but not in development. the `rejectUnauthorized: false`  is required for deployment to heroku etc...
-    });
-    let database = new SqlDatabase(new PostgresDataProvider(pool));
-    verifyStructureOfAllEntities(database); //This method can be run in the install phase on the server.
-    dataProvider = database;
-}
-
-let app = express();
-app.use(jwt({ secret: process.env.TOKEN_SIGN_KEY, credentialsRequired: false, algorithms: ['HS256'] }));
-app.use(compression());
-if (!process.env.DEV_MODE)
-    app.use(forceHttps);
-initExpress(app, {
-    dataProvider
-});
-app.use(express.static('dist/<%= project %>'));
-app.use('/*', async (req, res) => {
-    try {
-        res.send(fs.readFileSync('dist/<%= project %>/index.html').toString());
-    } catch (err) {
-        res.sendStatus(500);
+    // use json db for dev, and postgres for production
+    if (!process.env.DEV_MODE) {
+        const pool = new Pool({
+            connectionString: process.env.DATABASE_URL,
+            ssl: process.env.DEV_MODE ? false : { rejectUnauthorized: false }// use ssl in production but not in development. the `rejectUnauthorized: false`  is required for deployment to heroku etc...
+        });
+        let database = new SqlDatabase(new PostgresDataProvider(pool));
+        await verifyStructureOfAllEntities(database); 
+        dataProvider = database;
     }
-});
-let port = process.env.PORT || 3000;
-app.listen(port);
+
+    let app = express();
+    app.use(jwt({ secret: process.env.TOKEN_SIGN_KEY, credentialsRequired: false, algorithms: ['HS256'] }));
+    app.use(compression());
+    if (!process.env.DEV_MODE)
+        app.use(forceHttps);
+    initExpress(app, {
+        dataProvider
+    });
+    app.use(express.static('dist/<%= project %>'));
+    app.use('/*', async (req, res) => {
+        try {
+            res.send(fs.readFileSync('dist/<%= project %>/index.html').toString());
+        } catch (err) {
+            res.sendStatus(500);
+        }
+    });
+    let port = process.env.PORT || 3000;
+    app.listen(port);
+}
+startup();
