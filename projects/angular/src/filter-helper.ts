59a9294f0721b7e7a0d3e0fc99b31e7850256db6
Noam <noam@fireflymigration.com>
2021-08-21 11:35:08 +0300

    released 0.2.2

--- projects/angular/src/filter-helper.ts
+++ projects/angular/src/filter-helper.ts
@@ -1,9 +1,9 @@
 
 import { AndFilter, Filter } from 'remult';
-import { ComparisonFilterFactory, FieldRef, EntityWhere, FindOptions, Repository, ContainsFilterFactory } from "remult";
+import { ComparisonFilterFactory, FieldRef, EntityWhere, FindOptions, Repository, ContainsFilterFactory, getEntityRef } from "remult";
 import { FieldMetadata } from "remult";
 import { getFieldDefinition } from '..';
-import { getEntityRef, getEntitySettings } from '../../core/src/remult3';
+import { getEntitySettings } from 'remult/src/remult3';
 
 export class FilterHelper<rowType> {
   filterRow: rowType;
