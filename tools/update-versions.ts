91be1a22a9d55147b56e925f9b0c9bad60b9c437
Noam <noam@fireflymigration.com>
2021-09-13 12:26:35 +0300

    worked on release workflow

---
+++ tools/update-versions.ts
@@ -0,0 +1,14 @@
+import * as fs from 'fs';
+var root: packageJson = JSON.parse(fs.readFileSync("./package.json").toString());
+updateVersion('./dist/remult');
+updateVersion('./dist/angular');
+
+function updateVersion(path: string) {
+    var p: packageJson = JSON.parse(fs.readFileSync(path + "/package.json").toString());
+    p.version = root.version;
+    fs.writeFileSync(path + '/package.json', JSON.stringify(p, null, 4));
+}
+
+interface packageJson {
+    version: string;
+}
