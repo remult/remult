6327e4e5ac7c01b6e42fbce50e2199f45103b6d9
Yoni Rapoport <yoni@fireflymigration.com>
2022-04-17 16:18:00 +0300

    run jest from root for backend spec

--- projects/core/jest.config.js
+++ projects/core/jest.config.js
@@ -2,5 +2,10 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testRegex: "(.*).backend-spec.ts"
+  testRegex: "(.*).backend-spec.ts",
+  globals: {
+    'ts-jest': {
+      tsconfig: 'projects/core/tsconfig.backend-spec.json'
+    }
+  }
 };
