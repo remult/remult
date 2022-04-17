f9bb096ce3a02429e3c2691fbad9e99d2eab7df9
Noam Honig <noam.honig@gmail.com>
2022-04-17 16:13:06 +0300

    updated remult

---
+++ tools/update-remult-to-starter-projects.sh
@@ -0,0 +1,17 @@
+cd /repos/remult-react-todo
+npm i remult@latest
+git add .
+git commit -m "updated remult"
+git push
+
+cd /repos/remult-angular-todo
+npm i remult@latest
+git add .
+git commit -m "updated remult"
+git push
+
+cd /repos/angular-starter-project
+npm i remult@latest
+git add .
+git commit -m "updated remult"
+git push
