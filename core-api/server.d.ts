e1ebeaa0704576214ddb2f56df1186a8b85b9947
Noam Honig <noam.honig@gmail.com>
2022-07-27 20:55:52 +0300

    cleanup of api cache

--- core-api/server.d.ts
+++
@@ -1,1 +0,0 @@
-export { GenericRequest, GenericRequestHandler, GenericResponse, GenericRouter, buildRemultServer, RemultServerOptions, SpecificRoute, RemultServer } from './server/expressBridge';
--- core-api/server/register-actions-on-server.d.ts
+++
@@ -1,3 +0,0 @@
-import { SiteArea } from "./expressBridge";
-import { ClassType } from "../classType";
-export declare function registerActionsOnServer(area: SiteArea, controllers?: ClassType<any>[]): void;
