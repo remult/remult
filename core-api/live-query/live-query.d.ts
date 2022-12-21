c8928a0955732fd31e6bc49ce6cb3ab64f2499d7
Noam Honig <noam.honig@gmail.com>
2022-12-21 17:10:00 +0200

    core api cleanup

--- core-api/live-query/live-query.d.ts
+++
@@ -1,3 +0,0 @@
-export { LiveQueryPublisher, ServerEventDispatcher } from "./src/live-query/LiveQueryPublisher";
-export { PubSubClient, LiveQueryProvider, LiveQueryClient } from './src/live-query/LiveQuerySubscriber';
-export {};
