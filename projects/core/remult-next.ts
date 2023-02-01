2bc8c08b3bcbe2d531a179c874ecacb92909d9e6
Noam Honig <noam.honig@gmail.com>
2023-02-01 09:04:09 +0200

    reverted change

--- projects/core/remult-next.ts
+++ projects/core/remult-next.ts
@@ -1,44 +1,37 @@
-import type {
-  GetServerSideProps,
-  GetServerSidePropsResult,
-  NextApiRequest,
-  PreviewData,
-  NextApiHandler
-} from "next"
-import type { ParsedUrlQuery } from "querystring"
-import { createRemultServer, RemultServer, RemultServerOptions } from "./server"
+import type { GetServerSideProps, GetServerSidePropsContext, GetServerSidePropsResult, NextApiRequest, NextApiResponse, PreviewData } from 'next'
+import type { ParsedUrlQuery } from 'querystring';
+import { createRemultServer, RemultServer, RemultServerOptions } from './server';
+
+
 
 export function remultNext(
-  options?: RemultServerOptions<NextApiRequest>
-): RemultServer &
-  NextApiHandler & {
-    getServerSideProps<
-      P extends { [key: string]: any } = { [key: string]: any },
-      Q extends ParsedUrlQuery = ParsedUrlQuery,
-      D extends PreviewData = PreviewData
-    >(
-      getServerPropsFunction: GetServerSideProps<P, Q, D>
-    ): GetServerSideProps<P, Q, D>
-  } {
-  let result = createRemultServer(options)
-  return Object.assign(
-    (req, res) => result.handle(req, res),
-    result,
-    {
-      getServerSideProps: (getServerPropsFunction) => {
-        return (context) => {
-          return new Promise<GetServerSidePropsResult<any>>((res, err) => {
-            result.withRemult(context, undefined!, async () => {
-              try {
-                let r = await getServerPropsFunction(context)
-                res(JSON.parse(JSON.stringify(r)))
-              } catch (e) {
-                err(e)
-              }
-            })
-          })
-        }
+  options?:
+    RemultServerOptions<NextApiRequest>,
+): RemultServer & {
+  getServerSideProps<
+    P extends { [key: string]: any } = { [key: string]: any },
+    Q extends ParsedUrlQuery = ParsedUrlQuery,
+    D extends PreviewData = PreviewData
+  >(
+    getServerPropsFunction: GetServerSideProps<P, Q, D>
+  ): GetServerSideProps<P, Q, D>
+} {
+  let result = createRemultServer(options);
+  return Object.assign(result, {
+    getServerSideProps: getServerPropsFunction => {
+      return (context) => {
+
+        return new Promise<GetServerSidePropsResult<any>>((res, err) => {
+          result.withRemult(context, undefined!, async () => {
+            try {
+              let r = await getServerPropsFunction(context);
+              res(JSON.parse(JSON.stringify(r)));
+            } catch (e) {
+              err(e);
+            }
+          });
+        });
       }
-    }
-  )
+    },
+  })
 }
