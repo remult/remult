afb0a4e9e441a8c0c8c19e16c1179759b269a2d9
Noam Honig <noam.honig@gmail.com>
2023-02-13 08:55:12 +0200

    hope this works

--- projects/core/server/core.ts
+++ projects/core/server/core.ts
@@ -1,3 +1,4 @@
 export { queuedJobInfo, QueueStorage, GenericRequest, GenericRequestHandler, GenericResponse, GenericRouter, createRemultServerCore, RemultServerOptions, SpecificRoute, RemultServer } from './expressBridge';
 export { JsonEntityFileStorage, JsonFileDataProvider } from './JsonEntityFileStorage';
 export { SseSubscriptionServer } from '../SseSubscriptionServer'
+export { DataProviderLiveQueryStorage } from '../live-query/data-provider-live-query-storage';
