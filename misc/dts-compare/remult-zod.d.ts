d9be547b269fab0856e9fda6a75128b6f49b4834
Noam Honig <noam.honig@gmail.com>
2023-07-03 11:15:02 +0300

    adjusted tests and schema

--- misc/dts-compare/remult-zod.d.ts
+++ misc/dts-compare/remult-zod.d.ts
@@ -1,4 +1,7 @@
-import { infer as zInfer, ZodType } from 'zod';
+import { ZodObject, ZodRawShape, infer as zInfer, ZodType } from 'zod';
 import { FieldOptions } from './src/column-interfaces';
+import { EntityInfoProvider } from './src/context';
+import { EntityOptions } from './src/entity';
 import { MemberType } from './src/remult3';
+export declare function zodEntity<zodType extends ZodRawShape>(key: string, zodType: ZodObject<zodType>, options: EntityOptions<zInfer<ZodObject<zodType>>>): ZodObject<zodType> & EntityInfoProvider<zInfer<ZodObject<zodType>>>;
 export declare function zodField<zodType extends ZodType>(zodType: zodType, ...options: (FieldOptions<any, zInfer<zodType>> | MemberType<InstanceType<zInfer<zodType>>>)[]): zodType;
