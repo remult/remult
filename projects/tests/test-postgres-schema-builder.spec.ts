d6f06f3f78793366bd966b70c8a228106d0b4d94
Noam Honig <noam.honig@gmail.com>
2023-08-24 12:20:55 +0300

    Added test for JY

---
+++ projects/tests/test-postgres-schema-builder.spec.ts
@@ -0,0 +1,28 @@
+import { expect, it, describe } from 'vitest'
+import { config } from 'dotenv'
+import { Entity, Fields, dbNamesOf, describeClass, remult } from '../core'
+import {
+  PostgresSchemaBuilder,
+  createPostgresDataProvider,
+} from '../core/postgres'
+config()
+describe('Test postgres schema builder', () => {
+  it('test creates table', async () => {
+    const db = await createPostgresDataProvider() //connection string is in a .env file placed in the `tests` folder with key `DATABASE_URL`
+    remult.dataProvider = db
+    const table = class {
+      id = ''
+      name = ''
+    }
+    describeClass(table, Entity('test1'), {
+      id: Fields.string(),
+      name: Fields.string(),
+    })
+    const repo = remult.repo(table)
+    const t = await dbNamesOf(repo)
+    await db.execute(`drop table if exists ${t.$entityName}`)
+    var sb = new PostgresSchemaBuilder(db)
+    await sb.createIfNotExist(repo.metadata)
+    expect(await repo.count()).toBe(0)
+  })
+})
