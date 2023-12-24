6de52338fd0b8d43b4ab21b10fd3efaa6f47e231
Noam Honig <noam.honig@gmail.com>
2023-12-24 12:10:40 +0200

    1231321

--- projects/core/src/remult3/remult3.ts
+++ projects/core/src/remult3/remult3.ts
@@ -727,7 +727,7 @@ expose live query storage in remult
 request optional - and depends on that run `getUser` & `initRequest`
 */
 //y1 - What I would like is to make a copy of the item (without getting it from the server again)
-//y1 - from the crm-demo(https://crm-demo.up.railway.app/deals), after editing a deal:
+//y1 - from the crm-demo(https://crm-demo.up.railway.app/deals), after editing a deal: - _updateEntityBasedOnApi
 //y1 - consider moving the request to be second parameter of with remult async
 //y1 - Add a with remult promise to Remult!
 //y1 - Backend methods are transactions, it's not intuitive and maybe should be optional / opt in
