2da703b09541f64b9869c84c06397031a86e5132
jycouet <jycouet@gmail.com>
2024-01-28 19:39:53 +0100

    first step

---
+++ projects/remult-admin/src/App.svelte
@@ -0,0 +1,4 @@
+<script lang="ts">
+</script>
+
+<main>Welcome to Remult Admin</main>
--- projects/remult-admin/vite.config.ts
+++ projects/remult-admin/vite.config.ts
@@ -1,11 +1,29 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { viteSingleFile } from 'vite-plugin-singlefile'
-import express from 'vite3-plugin-express'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), viteSingleFile(), , express('src/server/')],
+  plugins: [
+    svelte(),
+    react(),
+    viteSingleFile(),
+    {
+      name: 'local-express',
+      configureServer: async (server: any) => {
+        server.middlewares.use(async (req: any, res: any, next: any) => {
+          process.env['VITE'] = 'true'
+          try {
+            const { app } = await server.ssrLoadModule('./src/server')
+            app(req, res, next)
+          } catch (err) {
+            console.error(err)
+          }
+        })
+      },
+    },
+  ],
 
   define: {
     __DEV__: process.env.DEV === 'true',
