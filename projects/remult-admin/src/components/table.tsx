be908807783742a46fe6d48e19e62f59028b3a1a
Ermin Celikovic <ermin@celikovic.me>
2024-01-29 14:13:09 +0100

    Show/Hide nav

--- projects/remult-admin/src/components/table.tsx
+++ projects/remult-admin/src/components/table.tsx
@@ -74,7 +74,7 @@ export function Table({
               nav?.classList.toggle('hide-navigation')
             }}
           >
-          <svg className="hamburger-icon" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><rect width="24" height="24" /><rect y="4.68134" width="24" height="2.03049" fill="black"/><rect y="10.9847" width="24" height="2.03049" fill="black"/><rect y="17.2882" width="24" height="2.03049" fill="black"/></svg>
+          <svg className="hamburger-icon" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><rect y="4.68134" width="24" height="2.03049" fill="black"/><rect y="10.9847" width="24" height="2.03049" fill="black"/><rect y="17.2882" width="24" height="2.03049" fill="black"/></svg>
           </button>
 
           <div className="page-bar__title">{repo.metadata.caption}</div>
