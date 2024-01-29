4a9a572a6858e10e7156fe5b973f04eb19eb2241
Ermin Celikovic <ermin@celikovic.me>
2024-01-29 13:40:32 +0100

    Collapsable sidebar

--- projects/remult-admin/src/components/table.tsx
+++ projects/remult-admin/src/components/table.tsx
@@ -62,9 +62,23 @@ export function Table({
     setOptions({ ...options, orderBy: { [key]: dir } })
   }
 
+
   return (
     <>
       <div className="page-bar">
+
+          <button
+            className="icon-button"
+            onClick={() => {
+              const nav = document.querySelector('body')
+              nav?.classList.toggle('hide-navigation')
+            }}
+          >
+          <svg className="hamburger-icon" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><rect width="24" height="24" /><rect y="4.68134" width="24" height="2.03049" fill="black"/><rect y="10.9847" width="24" height="2.03049" fill="black"/><rect y="17.2882" width="24" height="2.03049" fill="black"/></svg>
+          </button>
+
+          <div className="page-bar__title">{repo.metadata.caption}</div>
+
           <Filter
             fields={columns}
             filter={userFilter}
