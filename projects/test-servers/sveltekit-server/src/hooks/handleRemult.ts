eacf0e8ec2f497c9e5ff11cf32fe98342f0e4799
JYC <jycouet@gmail.com>
2024-02-01 19:08:08 +0100

    Kit and headers v2 (#334)
    * rmv the link to fav that is not deployed
    
    * small hack 😉
    
    * update kit test server to test headers & cookies

--- projects/test-servers/sveltekit-server/src/hooks/handleRemult.ts
+++ projects/test-servers/sveltekit-server/src/hooks/handleRemult.ts
@@ -1,7 +1,27 @@
-import { remultSveltekit } from 'remult/remult-sveltekit'
+import type { RequestEvent } from '@sveltejs/kit'
+import { remultSveltekit } from '../../../../core/remult-sveltekit'
+// import { remultSveltekit } from 'remult/remult-sveltekit'
 import { Task } from '../shared/Task'
+import { TasksController } from '../shared/TasksController'
+import { remult } from 'remult'
 
 export const handleRemult = remultSveltekit({
   entities: [Task],
+  controllers: [TasksController],
   admin: true,
+  initRequest: async (event) => {
+    remult.context.setHeaders = (headers) => {
+      event.setHeaders(headers)
+    }
+    remult.context.setCookie = (name, value) => {
+      event.cookies.set(name, value, { path: '.' })
+    }
+  },
 })
+
+declare module 'remult' {
+  export interface RemultContext {
+    setHeaders(headers: Record<string, string>): void
+    setCookie(...args: Parameters<RequestEvent['cookies']['set']>): void
+  }
+}
