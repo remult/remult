8aad6c8be577e1a2ce1862834f27812a9f463967
Noam Honig <noam.honig@gmail.com>
2024-04-01 11:53:57 +0300

    implemented async local storage fallback

--- projects/core/server/initAsyncHooks.ts
+++ projects/core/server/initAsyncHooks.ts
@@ -10,9 +10,17 @@ export function initAsyncHooks() {
   remultStatic.asyncContext = new RemultAsyncLocalStorage(
     new AsyncLocalStorageBridgeToRemultAsyncLocalStorageCore(),
   )
+  let test = new AsyncLocalStorage();
+  test.run(1, async () => {
+    await Promise.resolve()
+    if (test.getStore() === undefined) {
+      console.log("async_hooks.AsyncLocalStorage not working, using sequential implementation (You're probably running on stackblitz, this will work on a normal nodejs environment")
+      remultStatic.asyncContext = new RemultAsyncLocalStorage(new SequentialRemultAsyncLocalStorageCore())
+    }
+  })
 }
 
-export class AsyncLocalStorageBridgeToRemultAsyncLocalStorageCore<T> implements RemultAsyncLocalStorageCore<T>{
+export class AsyncLocalStorageBridgeToRemultAsyncLocalStorageCoreImpl<T> implements RemultAsyncLocalStorageCore<T>{
   private asyncLocalStorage = new AsyncLocalStorage<T>()
 
   wasImplemented: 'yes'
@@ -34,21 +42,47 @@ export class AsyncLocalStorageBridgeToRemultAsyncLocalStorageCore<T> implements 
   }
 
 }
+
+const ignoreInStack = ["AsyncLocalStorageBridgeToRemultAsyncLocalStorageCore.run", "RemultAsyncLocalStorage.run", "Module.withRemult", "RemultServerImplementation.runWithRemult"]
 export class SequentialRemultAsyncLocalStorageCore<T> implements RemultAsyncLocalStorageCore<T>{
   wasImplemented: 'yes'
   async run<R>(store: T, callback: () => Promise<R>): Promise<R> {
-    let result: R;
+
+    let log = (msg: string) => {
+      console.log(msg)
+    }
+    if (false) {
+      let stack = new Error().stack;
+      let y = stack.split('\n');
+      while (y.length > 0 && (y[0].trim().startsWith("Error") || ignoreInStack.includes(y[0].trim().split(' ')[1]))) {
+        y.splice(0, 1)
+      }
+      stack = y.join('\n');
+      log = (msg: string) => {
+        console.log(msg, stack)
+      }
+
+    }
+    log('waiting on ')
     const nextPromise = this.lastPromise.then(async () => {
+      log('executing  ')
       const previousValue = this.currentValue;
       this.currentValue = store;
-      this.lastPromise
+
       const result = await callback();
       this.currentValue = previousValue;
       return result;
     })
-    this.lastPromise = nextPromise.catch(() => undefined);
+    this.lastPromise = nextPromise.catch(() => {
+      log("Error on ")
+      return undefined
+    });
 
-    return await nextPromise;
+    try {
+      return await nextPromise;
+    } finally {
+      log('completed  ')
+    }
   }
 
   getStore(): T {
@@ -57,4 +91,10 @@ export class SequentialRemultAsyncLocalStorageCore<T> implements RemultAsyncLoca
 
   lastPromise: Promise<T> = Promise.resolve(undefined);
   currentValue: T;
+}
+
+
+export class AsyncLocalStorageBridgeToRemultAsyncLocalStorageCore<T> extends AsyncLocalStorageBridgeToRemultAsyncLocalStorageCoreImpl<T>{
+
+
 }
