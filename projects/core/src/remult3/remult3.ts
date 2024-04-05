8f8bbb7bbb6b1dd382199436503241bb44743b90
Noam Honig <noam.honig@gmail.com>
2024-04-05 08:59:52 +0300

    stop mongo no trans testing as it fails

--- projects/core/src/remult3/remult3.ts
+++ projects/core/src/remult3/remult3.ts
@@ -813,6 +813,7 @@ export type ClassFieldDecorator<entityType, valueType> = (
   c?: any,
 ) => void
 
+//p1 - fix validators cache
 //y1 - JY suggestion of having generate as part of the ensure schema - not sure
 //y1 - currently for string fields we default for 255 in knex (which is their default) why not just use text (varchar max) - and only use exact length when one is provided?
 //y1 - consider changing the sql raw filter to return the sql, instead of setting the sql property
