8cc6134b90c49a78e6e8884934a78305fc40c698
Noam <noam@fireflymigration.com>
2024-05-30 06:37:31 +0300

    updated remult

--- projects/core/src/remult3/remult3.ts
+++ projects/core/src/remult3/remult3.ts
@@ -831,8 +831,9 @@ export const flags = {
 
 //y1 TODO - discuss next auth withRemult paradox - the gift that keeps on giving - it's doing something crazy there!!
 //p1 - review starter and examples and separate remult * auth from the routes
-//p1 - https://github.com/remult/remult/discussions/438
-
+/*p1 - https://github.com/remult/remult/discussions/438
+     - https://github.com/remult/remult/blob/query-argumets/projects/tests/dbs/test-sql-database.spec.ts#L100-L128
+*/
 //y1 - live query with count #436
 
 //y1 TODO - In the esm version of our tutorial - the imports are automatically .ts and not .js in react and not in vue
