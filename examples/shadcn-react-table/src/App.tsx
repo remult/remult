59d6180dbb647e6c49f33945a85b030de24e74bc
Noam Honig <noam.honig@gmail.com>
2024-06-24 08:54:15 +0300

    more placeholders

--- examples/shadcn-react-table/src/App.tsx
+++ examples/shadcn-react-table/src/App.tsx
@@ -3,6 +3,7 @@ import TasksTable from './components/task-table/tasks-table.tsx'
 import { Button } from './components/ui/button.tsx'
 
 function App() {
+  async function click() {}
   return (
     <div className="flex space-x-2 p-4 flex-col">
       <div></div>
