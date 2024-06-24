dc9f9b93937557cb5edf14eb32e28f1eed7d6a93
Noam Honig <noam.honig@gmail.com>
2024-06-24 08:14:51 +0300

    added playground

--- examples/shadcn-react-table/src/App.tsx
+++ examples/shadcn-react-table/src/App.tsx
@@ -1,8 +1,11 @@
+import useQuestion from './components/dialog/useQuestion.tsx'
 import TasksTable from './components/task-table/tasks-table.tsx'
+import { Button } from './components/ui/button.tsx'
 
 function App() {
   return (
-    <div className="flex space-x-2 p-4">
+    <div className="flex space-x-2 p-4 flex-col">
+      <div></div>
       <TasksTable />
     </div>
   )
