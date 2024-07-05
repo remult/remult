d9e3ecfac56eed45c44b9da31c8a5975d8b73cf4
Noam Honig <noam.honig@gmail.com>
2024-07-05 09:32:02 +0300

    more work on sql relations

--- projects/tests/dbs/sql-stuff/sub-query/sql-relations.ts
+++ projects/tests/dbs/sql-stuff/sub-query/sql-relations.ts
@@ -1,6 +1,13 @@
-import { ClassType } from '../../../../core/classType.js'
-import { EntityFilter, SqlDatabase } from '../../../../core/index.js'
-import { ArrayItemType, subQuery } from './sub-query.js'
+import {
+  ClassType,
+  EntityDbNames,
+  EntityFilter,
+  SqlCommandWithParameters,
+  SqlDatabase,
+  dbNamesOf,
+  repo,
+} from '../../../../core/index.js' //replace with remult
+import { getRelationFieldInfo } from '../../../../core/internals.js' //replace with remult/internals
 
 export function sqlRelations<entityType>(
   forEntity: ClassType<entityType>,
@@ -8,53 +15,148 @@ export function sqlRelations<entityType>(
   return new Proxy(
     {},
     {
-      get: (target, relationField: keyof entityType & string) => {
-        return {
-          count: (
-            where?: EntityFilter<
-              ArrayItemType<entityType[keyof entityType & string]>
-            >,
-          ) => {
-            return new subQuery(forEntity).count(relationField, { where })
+      get: (target, relationField: keyof entityType & string) =>
+        new Proxy(new SqlRelationTools(forEntity, relationField), {
+          get: (target, prop: string) => {
+            if (prop == '$count') return target.$count
+            if (prop == '$subQuery') return target.$subQuery
+
+            return target.$fields[prop]
           },
-          get: (
-            field: keyof ArrayItemType<entityType[keyof entityType]> & string,
-          ) => {
-            return new subQuery(forEntity).get(relationField, field)
-          },
-          filterExists: (
-            where?: EntityFilter<
-              ArrayItemType<entityType[keyof entityType & string]>
-            >,
-          ) => {
-            return SqlDatabase.rawFilter(async (c) => {
-              return (
-                'exists ' +
-                (await new subQuery(forEntity).getSubQuery(
-                  relationField,
-                  () => '1',
-                  {
-                    where,
-                    c,
-                  },
-                ))
-              )
-            }) as EntityFilter<unknown>
-          },
-        } satisfies SqlRelationTool<ArrayItemType<entityType[keyof entityType]>>
-      },
+        }),
     },
   ) as SqlRelations<entityType>
 }
 
-export interface SqlRelationTool<toEntity> {
-  filterExists(where?: EntityFilter<toEntity>): EntityFilter<toEntity>
-  count(where?: EntityFilter<toEntity>): Promise<string>
-  get(field: keyof toEntity): Promise<string>
-}
-
 export type SqlRelations<entityType> = {
-  [p in keyof entityType]-?: SqlRelationTool<ArrayItemType<entityType[p]>>
+  [p in keyof entityType]-?: SqlRelation<ArrayItemType<entityType[p]>>
 }
 
-let x: Record<string, SqlRelations<any>> = {}
+export type SqlRelation<toEntity> = {
+  $count(where?: EntityFilter<toEntity>): Promise<string>
+  $subQuery(
+    what: (fieldNamesOfToEntity: EntityDbNames<toEntity>) => string,
+    options?: {
+      where?: EntityFilter<toEntity>
+      c?: SqlCommandWithParameters
+    },
+  ): Promise<string>
+} & {
+  [P in keyof toEntity]: Promise<string>
+}
+
+class SqlRelationTools<
+  myEntity,
+  relationKey extends keyof myEntity,
+  toEntity = ArrayItemType<myEntity[relationKey]>,
+> {
+  constructor(
+    private myEntity: ClassType<myEntity>,
+    private relationField: relationKey,
+  ) {}
+  $count = (where?: EntityFilter<toEntity>) => {
+    return this.$subQuery(() => 'count(*)', { where })
+  }
+
+  $fields = new Proxy(this, {
+    get: (target, field: keyof toEntity & string) => {
+      return target.$subQuery((fieldNamesOfToEntity) =>
+        fieldNamesOfToEntity.$dbNameOf(field),
+      )
+    },
+  }) as {
+    [P in keyof toEntity]: Promise<string>
+  }
+
+  $subQuery = async <relationKey extends keyof myEntity & string>(
+    what: (
+      fieldNamesOfToEntity: EntityDbNames<ArrayItemType<myEntity[relationKey]>>,
+    ) => string,
+    options?: {
+      where?: EntityFilter<toEntity>
+      c?: SqlCommandWithParameters
+    },
+  ) => {
+    const rel = getRelationFieldInfo(
+      repo(this.myEntity).fields.find(this.relationField as string),
+    )
+    if (!rel)
+      throw new Error(`${this.relationField as string} is not a relation`)
+    const relFields = rel.getFields()
+
+    let filters: string[] = []
+
+    const namesOfOtherTable: EntityDbNames<
+      ArrayItemType<myEntity[relationKey]>
+    > = await dbNamesOf(rel.toEntity, {
+      tableName: true,
+    })
+    const namesOfMyTable = await dbNamesOf(this.myEntity, { tableName: true })
+
+    for (const key in relFields.fields) {
+      if (Object.prototype.hasOwnProperty.call(relFields.fields, key)) {
+        filters.push(
+          `${namesOfOtherTable.$dbNameOf(
+            key as string,
+          )} = ${namesOfMyTable.$dbNameOf(relFields.fields[key] as string)}`,
+        )
+      }
+    }
+    let otherTableFilter = await SqlDatabase.filterToRaw(
+      repo(rel.toEntity),
+      options?.where,
+      undefined,
+      namesOfOtherTable,
+    )
+    if (otherTableFilter) filters.push(otherTableFilter)
+
+    return `(select ${what(namesOfOtherTable)} 
+      from ${namesOfOtherTable} 
+      where 
+      ${filters.join(' and ')}
+      )`
+  }
+}
+export type ArrayItemType<T> = T extends (infer U)[] ? U : T
+
+export function sqlRelationsFilter<entityType>(
+  forEntity: ClassType<entityType>,
+) {
+  return new Proxy(
+    {},
+    {
+      get: (target, relationField: keyof entityType & string) =>
+        new SqlRelationFilter(forEntity, relationField),
+    },
+  ) as {
+    [p in keyof entityType]-?: SqlRelationFilter<
+      entityType,
+      p,
+      ArrayItemType<entityType[p]>
+    >
+  }
+}
+
+export class SqlRelationFilter<
+  myEntity,
+  relationKey extends keyof myEntity,
+  toEntity = ArrayItemType<myEntity[relationKey]>,
+> {
+  private _tools: SqlRelationTools<myEntity, relationKey, toEntity>
+  constructor(myEntity: ClassType<myEntity>, relationField: relationKey) {
+    this._tools = new SqlRelationTools(myEntity, relationField)
+  }
+
+  some(where?: EntityFilter<toEntity>): EntityFilter<toEntity> {
+    //many orms use some, every, none
+    return SqlDatabase.rawFilter(async (c) => {
+      return (
+        'exists ' +
+        (await this._tools.$subQuery(() => '1', {
+          where,
+          c,
+        }))
+      )
+    })
+  }
+}
