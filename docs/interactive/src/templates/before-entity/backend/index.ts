42b3b500ed07e5877f257fae46ab24ab8cb514b3
Noam Honig <noam.honig@gmail.com>
2024-08-18 16:41:30 +0300

    updated tutorial to delete data before reaching the step

--- docs/interactive/src/templates/before-entity/backend/index.ts
+++ docs/interactive/src/templates/before-entity/backend/index.ts
@@ -1,3 +1,8 @@
 import express from 'express'
+import fs from 'fs'
 
 export const app = express()
+if (fs.existsSync('./db/tasks.json')) {
+  fs.unlinkSync('./db/tasks.json')
+  console.log('Deleted tasks.json')
+}
