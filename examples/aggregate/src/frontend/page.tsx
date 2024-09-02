ac494420db0071fa66c379f04c241dacaf3e33a9
Noam Honig <noam.honig@gmail.com>
2024-09-02 13:56:53 +0300

    start of demo

--- examples/aggregate/src/frontend/page.tsx
+++ examples/aggregate/src/frontend/page.tsx
@@ -4,13 +4,27 @@ import { Employee } from '../shared/model'
 
 export function Todo() {
   const [result, error] = usePromise(() => {
-    return repo(Employee).aggregate({})
+    return repo(Employee).find({
+      where: {
+        salary: {
+          $gt: 2000,
+        },
+      },
+    })
   }, [])
   return (
     <div>
       <h1>Aggregate</h1>
       <main>
-        <div>count:{result?.$count}</div>
+        <div>
+          <ul>
+            {result?.map((x) => (
+              <li>
+                {x.name} ({x.salary})
+              </li>
+            ))}
+          </ul>
+        </div>
 
         {error && <div>Error: {error.message}</div>}
       </main>
