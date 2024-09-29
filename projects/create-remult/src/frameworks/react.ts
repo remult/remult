81bb13954b60f5020325b75c6edfb404401fd7e3
Noam Honig <noam.honig@gmail.com>
2024-09-29 14:02:23 +0300

    updated readme

--- projects/create-remult/src/frameworks/react.ts
+++ projects/create-remult/src/frameworks/react.ts
@@ -28,7 +28,7 @@ export const react: Framework = {
         withPlugin: false,
       }),
     );
-    let components: string[] = [framework.name, "Vite"];
+    let components: string[] = [framework.display, "Vite"];
     if (server.display) components.push(server.display);
     components.push(db.display);
 
@@ -79,6 +79,76 @@ function App() {
   );
 }
 export default App;
+`,
+    );
+    fs.writeFileSync(
+      path.join(root, "README.md"),
+      `# ${projectName}
+
+## Getting Started
+
+### Includes
+
+${components.map((c) => `- ` + c).join("\n")}
+
+
+### Prerequisites
+
+Make sure you have the following installed on your machine:
+
+- [Node.js](https://nodejs.org/) (v20 or later)
+- [npm](https://www.npmjs.com/) (comes with Node.js)
+
+### Installation
+
+1. Install dependencies:
+
+   ${"```"}bash
+   npm install
+   ${"```"}
+
+### Running the Development Environment
+
+To develop locally, you'll need to run both the frontend and backend environments. This requires two terminal windows.
+
+${
+  server.requiresTwoTerminal
+    ? `1. In **Terminal 1**, run the frontend development server:
+
+   ${"```"}bash
+   npm run dev
+   ${"```"}
+
+   This will start the frontend development environment and automatically open your app in the browser.
+
+2. In **Terminal 2**, run the backend development server:
+
+   ${"```"}bash
+   npm run dev-node
+   ${"```"}
+
+   This will start the backend in watch mode, automatically restarting on code changes.
+`
+    : `1. run the development server:
+
+   ${"```"}bash
+   npm run dev
+   ${"```"}`
+}
+### Additional Scripts
+
+- **Build for production**:
+
+  ${"```"}bash
+  npm run build
+  ${"```"}
+
+- **Start the production server**:
+
+  ${"```"}bash
+  npm run start
+  ${"```"}
+
 `,
     );
   },
