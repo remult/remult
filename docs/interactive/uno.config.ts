75d5b6d92fc1cc3983583bde670be492ac86afa3
Noam Honig <noam.honig@gmail.com>
2024-10-24 09:37:51 +0300

    updated icons

--- docs/interactive/uno.config.ts
+++ docs/interactive/uno.config.ts
@@ -1,5 +1,13 @@
 import { defineConfig } from '@tutorialkit/theme'
+import { presetIcons } from 'unocss'
 
 export default defineConfig({
+  presets: [
+    presetIcons({
+      collections: {
+        ri: () => import('@iconify-json/ri').then((i) => i.icons), // Add this line
+      },
+    }),
+  ],
   // add your UnoCSS config here: https://unocss.dev/guide/config-file
 })
