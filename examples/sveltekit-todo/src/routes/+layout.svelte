59db50d56095317dd606e60061da6ad7d121158e
jycouet <jycouet@gmail.com>
2024-12-19 13:46:29 +0100

    bump todo example

--- examples/sveltekit-todo/src/routes/+layout.svelte
+++ examples/sveltekit-todo/src/routes/+layout.svelte
@@ -1,17 +1,30 @@
 <script lang="ts">
-  import { remult } from "remult";
-  import "../app.css"
+  import { Remult, remult } from 'remult'
+  import '../app.css'
+  import { createSubscriber } from 'svelte/reactivity'
 
   interface Props {
-    data: import('./$types').LayoutData;
-    children?: import('svelte').Snippet;
+    data: import('./$types').LayoutData
+    children?: import('svelte').Snippet
   }
 
-  let { data, children }: Props = $props();
+  let { data, children }: Props = $props()
 
-  $effect(() => {
-    remult.user = data.user;
-  });
+  remult.user = data.user
+
+  function initRemultSvelteReactivity() {
+    Remult.entityRefInit = (x) => {
+      let update = () => {}
+      let s = createSubscriber((u) => {
+        update = u
+      })
+      x.subscribe({
+        reportObserved: () => s(),
+        reportChanged: () => update(),
+      })
+    }
+  }
+  initRemultSvelteReactivity()
 </script>
 
 <svelte:head>
