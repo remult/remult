0894e9f9259506e3c93a59500f1ebcd72986eb32
Ermeen <celikovic@gmail.com>
2025-01-24 09:50:50 +0100

    Init editor component

--- docs/components/Editor.vue
+++ docs/components/Editor.vue
@@ -1,1 +1,45 @@
-<template>Editor...</template>
+<template>
+
+    <div class="editor">
+        <div class="editor-header">Customer Portal</div>
+        <div class="editor-body">
+
+            <div class="editor-sidebar">
+                <span>Commit History</span>
+                <button>define a model</button>
+                <button>add validation</button>
+                <button>pre-save hook</button>
+            </div>
+
+            <div class="editor-content">
+                <div class="editor-tabs">
+                    <button>Customer.ts</button>
+                    <button>CustomerList.tsx</button>
+                </div>
+
+                <div class="editor-code">
+                    Shiki here
+                </div>
+
+                <div class="editor-footer">
+                    <a href="/docs">More about validation</a>
+                    <a href="/docs">More about auth</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+
+<style>
+    .editor {
+        background: #050638;
+        border-radius: 3px;
+    }
+
+    .editor-header {
+        display: flex;
+        justify-content: center;
+    }
+</style>
+
--- docs/components/Homepage.vue
+++ docs/components/Homepage.vue
@@ -3,6 +3,48 @@ import Editor from './Editor.vue'
 </script>
 
 <template>
-  <h1>Home page...</h1>
-  <Editor />
+  <div class="intro">
+    <div class="title">
+      <h1>Adding full to your stack</h1>
+
+      <div class="cta">
+        <a href="/">Try in Browser</a>
+        <a href="/docs">Documentation</a>
+      </div>
+    </div>
+
+    <Editor />
+  </div>
 </template>
+
+
+<style>
+  .intro {
+    max-width: 800px;
+    width: 100%;
+    margin: 0 auto;
+  }
+  
+  .title {
+    text-align: center;
+    margin-bottom: 2rem;
+  }
+
+  h1 {
+    font-size: 2rem;
+    margin-bottom: 2rem;
+  }
+
+  .cta {
+    display: flex;
+    justify-content: center;
+    gap: 1rem;
+  }
+
+  .cta a {
+    display: inline-block;
+    background: #0D0D5B;
+    color: #fff;
+    padding: .5rem 2rem;
+  }
+</style>
