a387a19caa2f09fe61d06dd7695c7df16fa7004f
jycouet <jycouet@gmail.com>
2025-02-10 21:59:13 +0100

    for all

---
+++ projects/test-servers/shared/modules/someRoutes.ts
@@ -0,0 +1,85 @@
+import path from 'path'
+import fs from 'fs'
+import { Module } from '../../../core/server/index.js'
+
+export const someRoutes = new Module({
+  key: 'some-routes',
+  extraRoutes: ({ add, rootPath }) => {
+    // To move in remult ?! add in extraRoutes args ?
+    function addStaticFolder(
+      add: any,
+      rootPath: string,
+      urlPath: string,
+      folderPath: string,
+    ) {
+      add(urlPath).get((req: any, res: any) => {
+        const currentHttpBasePath = `${rootPath}${urlPath.replace('*', '')}`
+        const currentFilePath = folderPath
+
+        let filePath = req.url.pathname.replace(
+          currentHttpBasePath,
+          currentFilePath,
+        )
+
+        if (fs.existsSync(filePath)) {
+          if (fs.statSync(filePath).isDirectory()) {
+            const isIndexHtml = path.join(filePath, 'index.html')
+            if (!fs.existsSync(isIndexHtml)) {
+              filePath = filePath + '.html'
+            } else {
+              filePath = isIndexHtml
+            }
+          }
+        } else {
+          filePath = filePath + '.html'
+        }
+
+        try {
+          const content = fs.readFileSync(filePath, 'utf-8')
+
+          const seg = filePath.split('.')
+          const map: Record<string, string> = {
+            js: 'text/javascript',
+            css: 'text/css',
+            svg: 'image/svg+xml',
+            html: 'text/html',
+          }
+
+          res.send(content, {
+            'content-type': map[seg[seg.length - 1]] ?? 'text/plain',
+          })
+          return
+        } catch (error) {}
+
+        res.status(404).end()
+      })
+    }
+
+    add('/new-route').get((req, res) => {
+      console.log('extraRoutes /api/new-route')
+      res.json({ Soooooo: 'Cool! A new new-route!' })
+    })
+    add('/html').get((req, res) => {
+      res.send('<h1>Hello World</h1>')
+    })
+    add('/redirect').get((req, res) => {
+      res.redirect(307, '/api/html')
+    })
+    add('/redirect-ext').get((req, res) => {
+      res.redirect(
+        307,
+        'https://developer.mozilla.org/en-US/docs/Web/HTTP/Status#redirection_messages',
+      )
+    })
+    add('/setCookie').get((req, res) => {
+      res.setCookie('res.setCookie', 'Plop')
+      res.send('<h1>setCookie</h1><a href="/api/deleteCookie">deleteCookie</a>')
+    })
+    add('/deleteCookie').get((req, res) => {
+      res.deleteCookie('res.setCookie')
+      res.send('<h1>deleteCookie</h1><a href="/api/setCookie">setCookie</a>')
+    })
+
+    addStaticFolder(add, rootPath, '/styled*', './src/server/styled')
+  },
+})
--- projects/test-servers/sveltekit-server/src/server/api.ts
+++ projects/test-servers/sveltekit-server/src/server/api.ts
@@ -2,10 +2,8 @@ import { type RequestEvent } from '@sveltejs/kit'
 import { Task } from '../shared/Task'
 import { TasksController } from '../shared/TasksController'
 import { remult } from 'remult'
-import { Module } from 'remult/server'
 import { remultSveltekit } from 'remult/remult-sveltekit'
-import path from 'path'
-import fs from 'fs'
+import { someRoutes } from '../../../shared/modules/someRoutes'
 
 export const api = remultSveltekit({
   entities: [Task],
@@ -23,92 +21,8 @@ export const api = remultSveltekit({
     console.log('Ready 💪')
   },
 
-  modules: [
-    new Module({
-      key: 'some-routes',
-      extraRoutes: ({ add, rootPath }) => {
-        function addStaticFolder(
-          add: any,
-          rootPath: string,
-          urlPath: string,
-          folderPath: string,
-        ) {
-          add(urlPath).get((req: any, res: any) => {
-            const currentHttpBasePath = `${rootPath}${urlPath.replace('*', '')}`
-            const currentFilePath = folderPath
-
-            let filePath = req.url.pathname.replace(
-              currentHttpBasePath,
-              currentFilePath,
-            )
-
-            if (fs.existsSync(filePath)) {
-              if (fs.statSync(filePath).isDirectory()) {
-                const isIndexHtml = path.join(filePath, 'index.html')
-                if (!fs.existsSync(isIndexHtml)) {
-                  filePath = filePath + '.html'
-                } else {
-                  filePath = isIndexHtml
-                }
-              }
-            } else {
-              filePath = filePath + '.html'
-            }
-
-            try {
-              const content = fs.readFileSync(filePath, 'utf-8')
-
-              const seg = filePath.split('.')
-              const map: Record<string, string> = {
-                js: 'text/javascript',
-                css: 'text/css',
-                svg: 'image/svg+xml',
-                html: 'text/html',
-              }
-
-              res.send(content, {
-                'content-type': map[seg[seg.length - 1]] ?? 'text/plain',
-              })
-              return
-            } catch (error) {}
-
-            res.status(404).end()
-          })
-        }
-
-        add('/new-route').get((req, res) => {
-          console.log('extraRoutes /api/new-route')
-          res.json({ Soooooo: 'Cool! A new new-route!' })
-        })
-        add('/html').get((req, res) => {
-          res.send('<h1>Hello World</h1>')
-        })
-        add('/redirect').get((req, res) => {
-          res.redirect(307, '/api/html')
-        })
-        add('/redirect-ext').get((req, res) => {
-          res.redirect(
-            307,
-            'https://developer.mozilla.org/en-US/docs/Web/HTTP/Status#redirection_messages',
-          )
-        })
-        add('/setCookie').get((req, res) => {
-          res.setCookie('res.setCookie', 'Plop')
-          res.send(
-            '<h1>setCookie</h1><a href="/api/deleteCookie">deleteCookie</a>',
-          )
-        })
-        add('/deleteCookie').get((req, res) => {
-          res.deleteCookie('res.setCookie')
-          res.send(
-            '<h1>deleteCookie</h1><a href="/api/setCookie">setCookie</a>',
-          )
-        })
-
-        addStaticFolder(add, rootPath, '/styled*', './src/server/styled')
-      },
-    }),
-  ],
+  // TODO JYC: I need help to fix this!
+  modules: [someRoutes],
 })
 
 declare module 'remult' {
