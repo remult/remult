72abdcd9b9161a1dc9873cb54ad6e6438e3178f6
jycouet <jycouet@gmail.com>
2025-03-19 12:49:47 +0100

    having fun with naming

--- docs/components/homepage/Intro.vue
+++ docs/components/homepage/Intro.vue
@@ -3,27 +3,30 @@ import Editor from '../Editor.vue'
 </script>
 
 <template>
-    <div class="intro">
-      <div class="title">
-        <h1>Adding full to your stack</h1>
-        
-        <div class="cta">
-          <a href="/">Try in Browser</a>
-          <a href="/docs">Documentation</a>
-        </div>
-      </div>
-      
-      <div class="editor-container">
-        <Editor />
+  <div class="intro">
+    <div class="title">
+      <h1>
+        Adding full to your <i><small>full-</small></i
+        >stack
+      </h1>
+
+      <div class="cta">
+        <a href="/">Try in Browser</a>
+        <a href="/docs">Documentation</a>
       </div>
     </div>
+
+    <div class="editor-container">
+      <Editor />
+    </div>
+  </div>
 </template>
 
 <style>
-    .intro {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-    }
+.intro {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
 </style>
