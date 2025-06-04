99977f1f181c36125fade3a40ef3d9bc754c5c34
jycouet <jycouet@gmail.com>
2025-06-04 18:14:10 +0200

    better types ?

--- projects/test-servers/sveltekit-server/src/server/api.ts
+++ projects/test-servers/sveltekit-server/src/server/api.ts
@@ -2,6 +2,8 @@ import { remultApi } from 'remult/remult-sveltekit'
 import { someRoutes } from '../../../shared/modules/someRoutes/server.js'
 import { resBackendMethodModule } from '../../../shared/modules/resBackendMethod/server.js'
 import { taskModule } from '../../../shared/modules/task/server.js'
+import type { RequestEvent } from '@sveltejs/kit'
+import type { Module } from 'remult/server'
 
 export const api = remultApi({
   admin: true,
@@ -11,9 +13,10 @@ export const api = remultApi({
   },
 
   modules: [
-    // JYC TODO: type!
     someRoutes,
     resBackendMethodModule,
     taskModule,
-  ],
+  ] as Module<RequestEvent>[], 
+  // Maybe there is some better way to do this?
+  // as we are in remultApi of sveltekit... We should know it already, no?!
 })
