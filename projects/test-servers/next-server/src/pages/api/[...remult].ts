69c18a195b5bdeda3e509c5194e4a9f17ac80adf
jycouet <jycouet@gmail.com>
2025-06-05 23:29:08 +0200

    prepare step5

--- projects/test-servers/next-server/src/pages/api/[...remult].ts
+++ projects/test-servers/next-server/src/pages/api/[...remult].ts
@@ -1,7 +1,9 @@
 import { remultNext } from 'remult/remult-next'
 import { Task } from '../../shared/Task'
+import { someRoutes } from '../../../../shared/modules/someRoutes/server.js'
 
 export default remultNext({
   entities: [Task],
   admin: true,
+  modules: [someRoutes as any],
 })
