99d5b24f7c6f16a49fadd8aaa6917c55a6c62a91
Tai Vo <tai.a.vo@gmail.com>
2025-06-12 23:27:21 -0700

    add d1 tests

---
+++ projects/tests/dbs/d1/d1.spec.ts
@@ -0,0 +1,75 @@
+/// <reference types="./worker-configuration.d.ts" />
+
+import { it, describe, expect, beforeEach, beforeAll, afterAll } from 'vitest'
+import { Remult, SqlDatabase } from '../../../core'
+
+import { SqlDbTests } from '../shared-tests/sql-db-tests.js'
+import type { DbTestProps } from '../shared-tests/db-tests-props.js'
+
+import { allDbTests } from '../shared-tests/index.js'
+
+import { D1BindingClient, D1DataProvider } from '../../../core/remult-d1.js'
+import { getPlatformProxy } from "wrangler"
+
+describe('d1', () => {
+  let closePlatformProxy: () => Promise<void>
+  let d1DataProvider: D1DataProvider
+  let db: SqlDatabase
+  let remult: Remult
+
+  beforeAll(async () => {
+    const { env, dispose } = await getPlatformProxy<Env>({})
+    d1DataProvider = new D1DataProvider(new D1BindingClient(env.D1_TEST_DB))
+
+    closePlatformProxy = dispose
+  })
+
+  afterAll(async () => closePlatformProxy())
+
+  beforeEach(async () => {
+    db = new SqlDatabase(d1DataProvider)
+    remult = new Remult(db)
+  })
+  const props: DbTestProps = {
+    getDb() {
+      return db
+    },
+    getRemult() {
+      return remult
+    },
+    createEntity: async (entity) => {
+      const repo = remult.repo(entity)
+      await db.execute(
+        'drop table if exists ' + db.wrapIdentifier(repo.metadata.dbName),
+      )
+      await db.ensureSchema([repo.metadata])
+      return repo
+    },
+  }
+
+  allDbTests(props)
+  SqlDbTests({ ...props })
+
+  it('start works', async () => {
+    await db.execute('drop table if exists x')
+    await db.execute('create table x (id int)')
+    await db.execute('insert into x values (1)')
+    const r = await db.execute('select * from x')
+    expect(r.rows).toMatchInlineSnapshot(`
+      [
+        {
+          "id": 1,
+        },
+      ]
+    `)
+    const c = db.createCommand()
+    expect((await c.execute(`select * from x where id=${c.param(1)}`)).rows)
+      .toMatchInlineSnapshot(`
+      [
+        {
+          "id": 1,
+        },
+      ]
+    `)
+  })
+})
