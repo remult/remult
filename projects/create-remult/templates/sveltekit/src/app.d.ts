dda7fd439ea4bef9492637cd6b8070fbdfc4fafc
jyc.dev <jycouet@gmail.com>
2025-06-28 12:33:21 +0200

    small init tweaks (#737)
    * small init tweak
    
    * css date & time
    
    * show only MUST env values to be set.
    
    * small sveltekit tweaks
    
    * npm init remult@latest

--- projects/create-remult/templates/sveltekit/src/app.d.ts
+++ projects/create-remult/templates/sveltekit/src/app.d.ts
@@ -1,13 +1,19 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 declare global {
-	namespace App {
-		// interface Error {}
-		// interface Locals {}
-		// interface PageData {}
-		// interface PageState {}
-		// interface Platform {}
-	}
+  namespace App {
+    // interface Error {}
+    // interface Locals {}
+    // interface PageData {}
+    // interface PageState {}
+    // interface Platform {}
+  }
 }
+
+// declare module "remult" {
+//   interface FieldOptions<entityType, valueType> {
+//     placeholderText?: string
+//   }
+// }
 
 export {};
