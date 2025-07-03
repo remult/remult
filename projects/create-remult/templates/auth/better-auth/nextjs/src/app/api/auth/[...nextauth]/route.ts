87d15cc131cad0c83650bd3da6cc004af0675b3a
jycouet <jycouet@gmail.com>
2025-07-04 01:11:48 +0200

    okay nextjs

--- projects/create-remult/templates/auth/better-auth/nextjs/src/app/api/auth/[...nextauth]/route.ts
+++ projects/create-remult/templates/auth/better-auth/nextjs/src/app/api/auth/[...nextauth]/route.ts
@@ -1,2 +1,4 @@
-import { handlers } from "../../../../demo/auth/server/auth";
-export const { GET, POST } = handlers;
+import { auth } from "../../../../demo/auth/server/auth";
+import { toNextJsHandler } from "better-auth/next-js";
+
+export const { GET, POST } = toNextJsHandler(auth.handler);
