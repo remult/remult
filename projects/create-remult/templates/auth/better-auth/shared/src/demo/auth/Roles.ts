ce541fd72cdbad483417f70bdb907d6bbce2a30a
jycouet <jycouet@gmail.com>
2025-07-08 08:37:25 +0200

    doc update

--- projects/create-remult/templates/auth/better-auth/shared/src/demo/auth/Roles.ts
+++ projects/create-remult/templates/auth/better-auth/shared/src/demo/auth/Roles.ts
@@ -1,6 +1,6 @@
 import { Roles_Auth } from "./authEntities.js";
 
-/** ALL ROLES of your application. */
+/** ALL ROLES of your application. [Learn more](https://remult.dev/docs/modules#roles) */
 export const Roles = {
   Admin: "admin",
   ...Roles_Auth,
