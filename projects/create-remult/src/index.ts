ee67c18a3d77fb6a39bff2ac978e24270fd89e41
jycouet <jycouet@gmail.com>
2025-07-29 11:29:25 +0200

    back to latest

--- projects/create-remult/src/index.ts
+++ projects/create-remult/src/index.ts
@@ -349,8 +349,8 @@ async function init() {
     pkg.name = packageName || getProjectName();
     pkg.dependencies = sortObject({
       ...pkg.dependencies,
-      // remult: "latest",
-      remult: "3.1.0-next.1",
+      remult: "latest",
+      // remult: "3.1.0-next.1",
       ...db.dependencies,
       ...safeServer.dependencies,
       ...authInfo?.dependencies?.(safeServerName),
