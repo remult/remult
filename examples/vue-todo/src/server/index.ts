9b6023462337b0dd763a81a7c2bb96c052de42bb
delebash <danelebash@hotmail.com>
2025-08-04 01:50:35 -0400

    express v5

--- examples/vue-todo/src/server/index.ts
+++ examples/vue-todo/src/server/index.ts
@@ -18,7 +18,7 @@ app.use(auth)
 app.use(api)
 
 app.use(express.static(path.join(__dirname, "../")))
-app.get("/{*any}", (_, res) => {
+app.get("/{*splat}", (_, res) => {
   res.sendFile(path.join(__dirname, "../", "index.html"))
 })
 
