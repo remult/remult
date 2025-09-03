1f2a71a414921e3d562e8ee7ca7eb4a7ed610ab5
jycouet <jycouet@gmail.com>
2025-09-03 14:11:53 +0200

    bumps

--- projects/remult-admin/src/server/index.ts
+++ projects/remult-admin/src/server/index.ts
@@ -1,6 +1,6 @@
 import express from 'express'
-import { api } from './api'
+import { api } from './api.js'
 
 export const app = express()
 // app.use('/yop_yop', api)
-app.use('/', api)
+app.use('/', api as express.RequestHandler)
