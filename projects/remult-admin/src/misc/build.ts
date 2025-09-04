11ff669e934d3d0505ea338bb2398521c960e492
jyc.dev <jycouet@gmail.com>
2025-09-04 15:04:12 +0200

    fix: remult admin bundler (#791)
    * bumps
    
    * tweak build logic
    
    * bumpsh

--- projects/remult-admin/src/misc/build.ts
+++ projects/remult-admin/src/misc/build.ts
@@ -2,10 +2,14 @@ import fs from 'fs'
 
 const html = fs.readFileSync('tmp/index.html', 'utf8')
 
+function escapeForTemplateLiteral(str: string): string {
+  return str.replace(/\\/g, '\\\\').replace(/`/g, '\\`').replace(/\$/g, '\\$')
+}
+
 fs.writeFileSync(
   '../core/server/get-remult-admin-html.ts',
   `export function getHtml(){
-  return ${JSON.stringify(html)}
+  return \`${escapeForTemplateLiteral(html)}\`
 }`,
   'utf8',
 )
