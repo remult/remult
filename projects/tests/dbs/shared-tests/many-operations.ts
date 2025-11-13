af0ec6e378681fcd6d42336865a7b785b84ea688
jycouet <jycouet@gmail.com>
2025-11-13 21:54:11 +0100

    nicer tests

--- projects/tests/dbs/shared-tests/many-operations.ts
+++ projects/tests/dbs/shared-tests/many-operations.ts
@@ -1,21 +1,35 @@
-import { describe, expect, it } from 'vitest'
-import { Entity, Fields } from '../../../core'
+import { beforeEach, describe, expect, it } from 'vitest'
+import { Entity, Fields, Repository } from '../../../core'
 import { describeClass } from '../../../core/src/remult3/classDescribers'
 import type { DbTestProps } from './db-tests-props'
 
 export function manyOperations({ createEntity }: DbTestProps) {
   describe('many operations', () => {
-    it('delete many', async () => {
+    let r: Repository<unknown>
+    beforeEach(async () => {
       let e = class {
         id!: number
+        name?: string
       }
       describeClass(e, Entity('many', { allowApiCrud: true }), {
         id: Fields.id(),
+        name: Fields.string(),
       })
-      let c = await createEntity(e)
-      await c.insert([{}, {}, {}])
-      expect(await c.count()).toBe(3)
-      expect(await c.deleteMany({ where: 'all' })).toBe(3)
+      r = await createEntity(e)
+    })
+
+    it('delete many', async () => {
+      await r.insert([{}, {}, {}])
+      expect(await r.count()).toBe(3)
+      expect(await r.deleteMany({ where: 'all' })).toBe(3)
+      expect(await r.count()).toBe(0)
+    })
+
+    it('update many', async () => {
+      await r.insert([{}, {}, {}])
+      expect(await r.count()).toBe(3)
+      expect(await r.updateMany({ where: 'all', set: { name: 'yo' } })).toBe(3)
+      expect(await r.count()).toBe(3)
     })
   })
 }
